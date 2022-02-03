7c4f4aa43734f936318d93926c55a28426556759
larrykimutai <larrykimutai@gmail.com>
2022-02-03 17:34:31 -0500

    changing Master code
    Changed master code to see how pull requests work when merging a changed master with a changed branch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!! This is a new branch called newBranch");
+        System.out.println("Hello World!! This is a new branch called newBranch" + " changing the master");
 
         /*
         boolean = true / false statement
