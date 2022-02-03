dd5fc845ac3389461f0fd094de8073e6ab0d97ce
larrykimutai <larrykimutai@gmail.com>
2022-02-03 17:37:30 -0500

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!! This is a new branch called newBranch" + "newBranch2");
+        System.out.println("Hello World!! This is a new branch" 
+                           + "called newBranch"
+                           + "newBranch2");
 
         /*
         boolean = true / false statement
